12ad6691e63d6a380d3b3c58223a1135a009da6f
jbrit <pro.ajibolaojo@gmail.com>
2021-05-06 03:47:31 +0100

    Modal implemented

--- src/components/GridCard.tsx
+++ src/components/GridCard.tsx
@@ -1,5 +1,12 @@
-import { Button, Grid } from "@material-ui/core";
-import React from "react";
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid,
+} from "@material-ui/core";
+import React, { useState } from "react";
 
 type responses = Array<string | number | null>;
 type responsesArray = Array<responses>;
@@ -8,15 +15,30 @@ interface Props {
 }
 
 const GridCard = ({ place }: Props) => {
+  const [open, setOpen] = useState(false);
   return (
     <Grid item xs={12} sm={6} md={4} lg={3}>
       <Button
+        onClick={() => setOpen(true)}
         style={{ paddingTop: "1rem", paddingBottom: "1rem" }}
         fullWidth
         variant="contained"
       >
         {place[0]}({place[1].length})
       </Button>
+      <Dialog
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"More Flight Information"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>flight content here...</DialogContentText>
+        </DialogContent>
+      </Dialog>
     </Grid>
   );
 };
