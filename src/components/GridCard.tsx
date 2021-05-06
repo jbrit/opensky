7bbcd57a3e9b348dc947c665ba2ae30a9e5eef3c
jbrit <pro.ajibolaojo@gmail.com>
2021-05-06 15:28:04 +0100

    arrival and depature tabs created

--- src/components/GridCard.tsx
+++ src/components/GridCard.tsx
@@ -1,26 +1,45 @@
 import {
   Button,
+  CircularProgress,
   Dialog,
   DialogContent,
   DialogTitle,
   Grid,
+  Tab,
   Table,
   TableBody,
   TableCell,
   TableHead,
   TableRow,
+  Tabs,
 } from "@material-ui/core";
 import React, { useState } from "react";
 
-type responses = Array<string | number | null>;
-type responsesArray = Array<responses>;
 interface Props {
   place: string[];
 }
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index } = props;
+
+  return <>{value === index && children}</>;
+}
+
 const GridCard = ({ place }: Props) => {
   const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(0);
   const [icao, name] = place;
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
     <Grid item xs={12} sm={6} md={4} lg={3}>
       <Button
@@ -41,7 +60,18 @@ const GridCard = ({ place }: Props) => {
           {"More Flight Information"}
         </DialogTitle>
         <DialogContent>
-          {/* <Table aria-label="simple table">
+          <Tabs
+            value={value}
+            indicatorColor="primary"
+            textColor="primary"
+            onChange={handleChange}
+            aria-label="disabled tabs example"
+          >
+            <Tab label="Arrivals" />
+            <Tab label="Depatures" />
+          </Tabs>
+
+          <Table aria-label="simple table">
             <TableHead>
               <TableRow>
                 <TableCell>icao24</TableCell>
@@ -53,21 +83,45 @@ const GridCard = ({ place }: Props) => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {details.map((detail) => (
-                <TableRow key={detail[0]}>
-                  <TableCell component="th" scope="row">
-                    {detail[0]}
-                  </TableCell>
-                  <TableCell align="right">{detail[1]}</TableCell>
-                  <TableCell align="right">{detail[2]}</TableCell>
-                  <TableCell align="right">{detail[3]}</TableCell>
-                  <TableCell align="right">{detail[4]}</TableCell>
-                  <TableCell align="right">{detail[5]}</TableCell>
-                </TableRow>
-              ))}
+              <TabPanel value={value} index={0}>
+                {/* Arrivals */}
+                {[0, 1, 2, 3].map((detail) => (
+                  <TableRow key={detail}>
+                    <TableCell component="th" scope="row">
+                      {detail}
+                    </TableCell>
+                    <TableCell align="right">detail 1</TableCell>
+                    <TableCell align="right">detail 2</TableCell>
+                    <TableCell align="right">detail 3</TableCell>
+                    <TableCell align="right">detail 4</TableCell>
+                    <TableCell align="right">detail 5</TableCell>
+                  </TableRow>
+                ))}
+                <CircularProgress
+                  style={{ margin: "auto", display: "none" }}
+                  disableShrink
+                />
+              </TabPanel>
+              <TabPanel value={value} index={1}>
+                {[0, 1, 2, 3].map((detail) => (
+                  <TableRow key={detail}>
+                    <TableCell component="th" scope="row">
+                      {detail}
+                    </TableCell>
+                    <TableCell align="right">depature 1</TableCell>
+                    <TableCell align="right">depature 2</TableCell>
+                    <TableCell align="right">depature 3</TableCell>
+                    <TableCell align="right">depature 4</TableCell>
+                    <TableCell align="right">depature 5</TableCell>
+                  </TableRow>
+                ))}
+                <CircularProgress
+                  style={{ margin: "auto", display: "none" }}
+                  disableShrink
+                />
+              </TabPanel>
             </TableBody>
           </Table>
-         */}
         </DialogContent>
       </Dialog>
     </Grid>
